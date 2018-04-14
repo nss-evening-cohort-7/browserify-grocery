85b283528e7b1b729268ad25ef54efbb8dbc886f
Zoe Ames <zoeames@gmail.com>
2018-04-14 11:58:17 -0500

    mouseenter and mouseleave working

---
+++ javascripts/events.js
@@ -0,0 +1,29 @@
+const departmentCards = document.getElementsByClassName('department');
+
+const showDepartmentName = (e) => {
+  const departmentImg = e.target.children[1];
+  const departmentName = e.target.children[0];
+
+  departmentName.classList.remove('hide');
+  departmentImg.classList.add('grey-out');
+};
+
+const hideDepartmentName = (e) => {
+  const departmentImg = e.target.children[1];
+  const departmentName = e.target.children[0];
+
+  departmentName.classList.add('hide');
+  departmentImg.classList.remove('grey-out');
+};
+
+const addDepartmentEvents = () => {
+  for (let i = 0; i < departmentCards.length; i++) {
+    departmentCards[i].addEventListener('mouseenter', showDepartmentName);
+    departmentCards[i].addEventListener('mouseleave', hideDepartmentName);
+    // departmentCards[i].addEventListener('click', showItems);
+  }
+};
+
+module.exports = {
+  addDepartmentEvents,
+};
