ae3715183f3759f37284118f662f900583def388
Zoe Ames <zoeames@gmail.com>
2018-04-16 19:46:27 -0500

    panels for items

--- javascripts/itemsDom.js
+++ javascripts/itemsDom.js
@@ -3,7 +3,19 @@ const itemsOutputDiv = document.getElementById('items');
 const itemsDomString = (items) => {
   let domString  = '';
   items.forEach((item) => {
-    domString  += `<h3>${item.name}</h3>`;
+    // domString  += `<h3>${item.name}</h3>`;
+    domString  += `<div class="col-sm-3 text-center item">`;
+    domString  +=   `<div class="panel panel-info">`;
+    domString  +=     `<div class="panel-heading">`;
+    domString  +=       `<h3 class="panel-title">${item.name}</h3>`;
+    domString  +=     `</div>`;
+    domString  +=     `<div class="panel-body">`;
+    domString  +=       `<div class="row">`;
+    domString  +=       `<img class="item-image" src="${item.img}">`;
+    domString  +=       `</div>`;
+    domString  +=     `</div>`;
+    domString  +=   `</div>`;
+    domString  += `</div>`;
   });
   return domString;
 };
