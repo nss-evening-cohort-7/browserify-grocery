773473dceeef9e3132acd9b6dddcc7778d7c0132
Zoe Ames <zoeames@gmail.com>
2018-04-17 19:53:52 -0500

    cart prints to table

--- javascripts/cartDom.js
+++ javascripts/cartDom.js
@@ -1,9 +1,32 @@
 const data = require('./data');
-// const cartOutputDiv = document.getElementById('cart');
+const cartOutputDiv = document.getElementById('cart');
+
+const cartDomString = (items) => {
+  let domString = '<h3 class="text-center">Your Cart</h3>';
+  domString += `<div class="col-xs-8 col-xs-offset-2">`;
+  domString += `<table class="table table-striped">`;
+  domString +=   `<tr>`;
+  domString +=     `<th>Name</th>`;
+  domString +=     `<th>Price</th>`;
+  domString +=     `<th>Quantity</th>`;
+  domString +=     `<th></th>`;
+  domString +=   `</tr>`;
+  items.forEach((item) => {
+    domString +=   `<tr>`;
+    domString +=     `<td>${item.name}</td>`;
+    domString +=     `<td>$${item.price.toFixed(2)}</td>`;
+    domString +=     `<td>${item.purchaseNum}</td>`;
+    domString +=     `<td>X</td>`;
+    domString +=   `</tr>`;
+  });
+  domString += `</table>`;
+  domString += `</div>`;
+  return domString;
+};
 
 const printCartToDom = () => {
   const cartItems = data.getCart();
-  console.log('cartItems', cartItems);
+  cartOutputDiv.innerHTML = cartDomString(cartItems);
 };
 
 module.exports = printCartToDom;
