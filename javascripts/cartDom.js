50dc60ea1c730a632850d856d2e47566e2022473
Zoe Ames <zoeames@gmail.com>
2018-04-17 20:13:08 -0500

    cart displays

--- javascripts/cartDom.js
+++ javascripts/cartDom.js
@@ -20,7 +20,25 @@ const cartDomString = (items) => {
     domString +=   `</tr>`;
   });
   domString += `</table>`;
+  domString += getTotals(items);
   domString += `</div>`;
+  return domString;
+};
+
+const getTotals = (items) => {
+  let itemTotal = 0;
+  let priceTotal = 0;
+  items.forEach((item) => {
+    itemTotal += (item.purchaseNum * 1);
+    priceTotal += (item.purchaseNum * item.price);
+  });
+  return totalsString(itemTotal, priceTotal);
+};
+
+const totalsString = (itemNum, total) => {
+  let domString = '';
+  domString += `<h4>Total number of items: ${itemNum}</h4>`;
+  domString += `<h4>Total price: $${total.toFixed(2)}</h4>`;
   return domString;
 };
 
